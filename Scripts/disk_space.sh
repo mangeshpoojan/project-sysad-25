15634e86409f52c9d596b0b6017a50860eb45370
mangeshpoojan <mangeshpoojan@gmail.com>
2025-11-12 20:07:17 +0530

    made some changes to disk_usuage script

--- Scripts/disk_space.sh
+++ Scripts/disk_space.sh
@@ -4,13 +4,10 @@ password="$(cat shadow)"
 
 #OUTFILE="disk_space_$(date +%F_%H%M%S).txt"
 OUTFILE="disk_space.txt"
-host=$2
 lab=$1
-remote="sl${lab}-${host}.cse.iitb.ac.in"
+host=$2
+remote="${lab}-${host}.cse.iitb.ac.in"
 
-result=$(sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=13 sysad@"$remote" "df -h / | awk 'NR==2 {print \$2, \$3, \$4, \$5}'")
+result=$(sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=13 sysad@"$remote" "df -h / | awk 'NR==2 {print \$2, \$3, \$4, \$5}'" 2>/dev/null)
 
-echo $result
-echo "$lab-$host $result"  >> "$OUTFILE"
-echo "Saved results in $OUTFILE"
-
+echo "$lab-$host $result" # >> "$OUTFILE"
