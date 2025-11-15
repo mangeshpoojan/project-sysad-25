d6fc850b5de662bdf2d256650ce9d3011879cf82
mangeshpoojan <mangeshpoojan@gmail.com>
2025-11-15 17:49:12 +0530

    completed the feature of uptimes

---
+++ Scripts/uptimes.py
@@ -0,0 +1,19 @@
+import subprocess
+import time
+
+lab = "sl1"
+subprocess.run(["bash", "status.sh", lab])
+
+with open("alive_hosts", "r") as f:
+    lines = f.readlines()
+
+output = []
+
+for line in lines:
+    # print(line)
+    sys_num = line.split("-")[1].split(".")[0]
+    result = subprocess.run(
+        ["bash", "uptimes.sh", lab, sys_num], capture_output=True, text=True
+    )
+    time.sleep(0.5)
+    output.append(str(result.stdout.strip()))
--- Scripts/uptimes.sh
+++ Scripts/uptimes.sh
@@ -4,13 +4,10 @@ password="$(cat shadow)"
 
 #OUTFILE="disk_space_$(date +%F_%H%M%S).txt"
 OUTFILE="uptimes.txt"
-host=$2
 lab=$1
-remote="sl${lab}-${host}.cse.iitb.ac.in"
+host=$2
+remote="${lab}-${host}.cse.iitb.ac.in"
 
-result=$(sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=13 sysad@"$remote" "uptime -p | awk '{print \$2, \$3, \$4, \$5, \$6, \$7}'")
+result=$(sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=13 sysad@"$remote" "uptime -p | awk '{print \$2, \$3, \$4, \$5, \$6, \$7}'" 2>/dev/null)
 
-echo $result
-echo "$lab-$host $result"  >> "$OUTFILE"
-echo "Saved results in $OUTFILE"
-
+echo "$lab-$host $result" # >> "$OUTFILE"
