12b07ee8e229d608f2193b54076d4383f068f4d4
umang <umang.jaiswal2003@gmail.com>
2025-11-24 14:14:36 +0530

    uploading

--- Database/bash_scripts/better_check.sh
+++ Database/bash_scripts/better_check.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-DIR="/home/sysad/Desktop/submission_25m0829"
-SESSION=sysad
-COURSE_CODE=cs699
-TEST_NO=1
-ROLLNO=25m0829
-FILENAME=/home/sysad/Desktop/sl1-2_submission_25m0829.tar.gz
 
 if [ -d "$DIR" ]; then
         if [ "$(ls -A $DIR)" ]; then
@@ -27,32 +20,3 @@ else
         exit 0
 fi
 
-SOURCE_DIR = "/home/$SESSION/Desktop"
-cd ${SOURCE_DIR}
-
-rollno=$(ls | grep "_submission_" | sed -n 's/^.*\([0-9]\{2\}[a-zA-Z][0-9]\{4\}\).*$/\1/p')
-
-echo $rollno
-
-host_parts=$(hostname | sed 's/-/ /')
-read lab seat_number <<< "$host_parts"
-
-echo "Lab Variable: $lab"
-echo "Seat Number Variable: $seat_number"
-
-if [ -f "$FILENAME" ]
-then
-    echo "Files that will be submitted are:"
-    echo "Files                    |  Size (bytes) | Timestamp"
-    cat /home/sysad/find_results.txt 
-    sshpass -p "sahilunagar" scp -o StrictHostKeyChecking=no $FILENAME cs683@10.9.100.47:/home/cs683/submissions
-    if [ $? -eq 0 ]
-    then
-      echo "successfully submitted !!"
-      # curl -d "StudentID=$rollno&CourseCode=$course_code&Lab=$lab&Subject=$subject&TestNo=$test_no&SeatNo=$seat_number&Submitted=True" -X POST http://localhost:5000/student/submit
-    else
-      echo "please retry submissioni!!"
-    fi
-else
-  echo "Please run check first!!"
-fi
