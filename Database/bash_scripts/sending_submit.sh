84c0daec12c87c5286423d5a837d9059428f04c3
umang <umang.jaiswal2003@gmail.com>
2025-11-26 12:46:55 +0530

    corrected footer link

--- Database/bash_scripts/sending_submit.sh
+++ Database/bash_scripts/sending_submit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-DIR="/home/sysad/Desktop/submission_25m0005"
+DIR="/home/sysad/Desktop/submission_25m0001"
 SESSION=sysad
-DESTINATION_PATH=/home/cs683/submissions/cs699/8
-COURSE_CODE=cs699
-TEST_NO=8
-ROLLNO=25m0005
-FILENAME=/home/sysad/Desktop/sl3-5_submission_25m0005.tar.gz
+DESTINATION_PATH=/home/cs683/submissions/cs683/3
+COURSE_CODE=cs683
+TEST_NO=3
+ROLLNO=25m0001
+FILENAME=/home/sysad/Desktop/sl3-2_submission_25m0001.tar.gz
 
 if [ -d "$DIR" ]; then
         if [ "$(ls -A $DIR)" ]; then
